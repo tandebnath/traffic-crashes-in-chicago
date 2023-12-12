88023db848206db6bdf9859b2689a6db7edbd4ea
Tanmoy Debnath <53893576+tandebnath@users.noreply.github.com>
2023-12-12 16:21:25 -0600

    Updated Sherry Viz

--- components/SherryViz/SherryViz.tsx
+++ components/SherryViz/SherryViz.tsx
@@ -2,7 +2,7 @@ import { VegaLite } from "react-vega";
 import styles from "./SherryViz.module.scss";
 
 const SherryViz = () => {
-  const spec1 = {
+  const spec = {
     title: "2019",
     data: {
       url: "https://raw.githubusercontent.com/chuzesta/chuzesta/main/crashes_2019.csv",
@@ -39,20 +39,43 @@ const SherryViz = () => {
     config: { background: "#FCF0E1" },
   };
 
-  const spec2 = {
-    title: "2022",
+  const spec1 = {
+    title: "Heatmap of 2019",
     data: {
-      url: "https://raw.githubusercontent.com/chuzesta/chuzesta/main/crashes_2022.csv",
+      url: "https://raw.githubusercontent.com/chuzesta/chuzesta/main/crashes_2019.csv",
     },
     mark: "rect",
     encoding: {
       x: {
         field: "CRASH_HOUR",
-        type: "quantitative",
-        axis: {
-          title: "Hour of the Day",
-          labelExpr: "datum.value + ':00'",
-        },
+        type: "ordinal",
+        axis: { title: "Hour of the Day", labelExpr: "datum.value + ':00'" },
+        sort: [
+          "0",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+        ],
       },
       y: {
         field: "CRASH_DAY_OF_WEEK",
@@ -66,14 +89,74 @@ const SherryViz = () => {
         title: "Number of Crashes",
       },
       tooltip: [
-        { field: "CRASH_HOUR", type: "quantitative", title: "Hour" },
+        { field: "CRASH_HOUR", type: "ordinal", title: "Hour" },
         { field: "CRASH_DAY_OF_WEEK", type: "ordinal", title: "Day of Week" },
         { aggregate: "count", title: "Number of Crashes" },
       ],
     },
-    width: 375,
+    config: { view: { stroke: "transparent" }, background: "#FCF0E1" },
+    width: 400,
     height: 300,
-    config: { background: "#FCF0E1" },
+  };
+
+  const spec2 = {
+    title: "Heatmap of 2022",
+    data: {
+      url: "https://raw.githubusercontent.com/chuzesta/chuzesta/main/crashes_2022.csv",
+    },
+    mark: "rect",
+    encoding: {
+      x: {
+        field: "CRASH_HOUR",
+        type: "ordinal",
+        axis: { title: "Hour of the Day", labelExpr: "datum.value + ':00'" },
+        sort: [
+          "0",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+        ],
+      },
+      y: {
+        field: "CRASH_DAY_OF_WEEK",
+        type: "ordinal",
+        axis: { title: "Day of the Week" },
+      },
+      color: {
+        aggregate: "count",
+        type: "quantitative",
+        scale: { scheme: "greenblue" },
+        title: "Number of Crashes",
+      },
+      tooltip: [
+        { field: "CRASH_HOUR", type: "ordinal", title: "Hour" },
+        { field: "CRASH_DAY_OF_WEEK", type: "ordinal", title: "Day of Week" },
+        { aggregate: "count", title: "Number of Crashes" },
+      ],
+    },
+    config: { view: { stroke: "transparent" }, background: "#FCF0E1" },
+    width: 400,
+    height: 300,
   };
 
   return (
